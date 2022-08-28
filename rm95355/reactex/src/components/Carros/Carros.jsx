6b3ac2f02e191000ef1eb322bd9128e29ca11788
AraujoAlan <aalan.araujo@hotmail.com>
2022-08-28 17:15:54 -0300

    termino do ex

---
+++ rm95355/reactex/src/components/Carros/Carros.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Carros(props) {
+  return (
+
+    <div>
+
+        <h2>Lista de Carros</h2>
+        <ul>
+            {props.listaCarrosProps.map((c,i)=>
+                <li key={i}>{i+1} - {c}</li>
+            )}
+        </ul>
+
+    </div>
+
+  )
+}
